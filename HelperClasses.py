a492d71d9b52dbbc6a03ddcce6ca142a938ab37d
fszafran <filo1234@interia.pl>
2024-11-04 21:45:22 +0100

    + zupelnie nie testowany a*, potrzebne poprawki w util klasach

--- HelperClasses.py
+++ HelperClasses.py
@@ -1,5 +1,8 @@
+from typing import List, Tuple, Set, Dict
+from enum import Enum
+
 class Edge:
-    def __init__(self, id, id_from, id_to, length, road_category, number_of_attractions):
+    def __init__(self, id: int, id_from: str, id_to: str, length: float, road_category: float, number_of_attractions: int):
         self.id = id
         self.id_from = id_from
         self.id_to = id_to
@@ -7,40 +10,55 @@ class Edge:
         self.road_category = road_category
         self.number_of_attractions = number_of_attractions
 
-    def __repr__(self):
+    def get_end(self) -> str:
+        return self.id_to
+    
+    def cost_length(self) -> float:
+        return self.length
+    
+    def cost_time(self) -> float:
+        return self.length / self.road_category
+    
+    def cost_attractions(self) -> int:
+        return self.number_of_attractions
+
+    def __repr__(self) -> str:
         return f"Edge {self.id} from {self.id_from} to {self.id_to}"
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.id_from, self.id_to, self.length))
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Edge') -> bool:
         return (self.id_from, self.id_to, self.length) == (other.id_from, other.id_to, other.length)
 
 class Node:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
         self.id = self.generate_id()
         self.edges = set()
 
-    def generate_id(self):
+    def generate_id(self) -> str:
         return f"{self.x}_{self.y}"
 
-    def add_edge(self, edge):
+    def add_edge(self, edge: Edge):
         self.edges.add(edge)
 
-    def __repr__(self):
+    def get_neighbours(self) -> List[Tuple[Edge, str]]:
+        return [(edge, edge.get_end()) for edge in self.edges]
+    
+    def __repr__(self) -> str:
         return f"Node {self.id}"
     
 class Graph:
     def __init__(self):
-        self.graph = {}
+        self.graph: Dict[str, Node] = {}
 
-    def add_node(self, node):
+    def add_node(self, node: Node):
         if node.id not in self.graph:
             self.graph[node.id] = node
 
-    def add_edge(self, edge):
+    def add_edge(self, edge: Edge):
         if edge.id_from in self.graph:
             self.graph[edge.id_from].add_edge(edge)
 
@@ -48,14 +66,6 @@ class Graph:
             reversed_edge = Edge(edge.id, edge.id_to, edge.id_from, edge.length, edge.road_category, edge.number_of_attractions)
             self.graph[edge.id_to].add_edge(reversed_edge)
 
-    def __repr__(self):
-        for key, value in self.graph.items():
-            print(f"{key}: {value.edges}")
-
-    def __str__(self):
-        return self.__repr__()
-
-from enum import Enum
 class Category(Enum):
     AUTOSTRADA = 140
     EKSPRESOWA = 120
@@ -65,11 +75,11 @@ class Category(Enum):
     LOKALNA = 50
     DOJAZDOWA = 30
     INNA = 20
-    BRAK_KATEGORII = 1
+    BRAK_KATEGORII = 5
 
 class CategoryFactory:
     @staticmethod
-    def get_category(category):
+    def get_category(category: str) -> Category:
         if category == "A":
             return Category.AUTOSTRADA
         elif category == "S":
---
+++ algorithm.py
@@ -0,0 +1,75 @@
+import numpy as np
+from HelperClasses import *
+from typing import List, Dict
+from queue import PriorityQueue
+import testGraphGenerator
+
+def heuristic(from_node_id: str, to_node_id: str) -> float:
+    x1, y1 = map(float, from_node_id.split("_"))
+    x2, y2 = map(float, to_node_id.split("_"))
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def get_appropriate_cost(edge: Edge, route_type: int) -> float:
+    if route_type == 0:
+        return edge.cost_time()
+    elif route_type == 1:
+        return edge.cost_length()
+    elif route_type == 2:
+        return edge.cost_attractions()
+    else:
+        raise ValueError("Invalid route type")
+
+def reconstruct_path(came_from: Dict[str, str], current_node_id: str) -> List[str]:
+    path = [current_node_id]
+    while current_node_id in came_from:
+        current_node_id = came_from[current_node_id]
+        path.append(current_node_id)
+    path.reverse()
+    return path
+
+def algorithm(graph: Graph, start: List[float], end: List[float], route_type: int) -> List[str]:
+    start_node = graph.graph[Node(start[0], start[1]).id]
+    end_node = graph.graph[Node(end[0], end[1]).id]
+
+    count = 0 # w razie remisow
+
+    open_set = PriorityQueue()
+    open_set.put((0, count, start_node))
+
+    came_from = {}
+    g_score = {node: float("inf") for node in graph.graph.keys()}
+    g_score[start_node.id] = 0
+
+    f_score = {node: float("inf") for node in graph.graph.keys()}
+    f_score[start_node.id] = heuristic(start_node.id, end_node.id)
+
+    open_set_hash = {start_node.id}
+
+    while not open_set.empty():
+        current_node: Node = open_set.get()[2]
+        open_set_hash.remove(current_node.id)
+
+        if current_node.id == end_node.id:
+            return reconstruct_path(came_from, current_node.id)
+
+        neighbours = current_node.get_neighbours()
+        for edge, neighbour_id in neighbours:
+            temp_g_score = g_score[current_node.id] + get_appropriate_cost(edge, route_type)
+            if temp_g_score < g_score[neighbour_id]:
+                came_from[neighbour_id] = current_node.id
+                g_score[neighbour_id] = temp_g_score
+                f_score[neighbour_id] = temp_g_score + heuristic(graph.graph[neighbour_id].id, end_node.id)
+                if neighbour_id not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbour_id], count, graph.graph[neighbour_id]))
+                    open_set_hash.add(neighbour_id)
+    return []  
+  
+if __name__ == "__main__":
+    graph = testGraphGenerator.generate_graph()
+    start = [0, 0]
+    end = [2, 2]
+    route_type = 0
+    print(algorithm(graph, start, end, route_type))
+    
+    
