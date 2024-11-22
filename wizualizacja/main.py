836ff3e0add1ac709006a5451fe3b4c9dceb2986
milenart <mikolaj.lenart17@gmail.com>
2024-11-22 20:34:27 +0100

    Usuniecie tworzenia nowego noda, poprawienie wizualizacji i usuwania drog

--- wizualizacja/main.py
+++ wizualizacja/main.py
@@ -3,6 +3,7 @@ from algorithm import algorithm
 from HelperClasses import Node
 import numpy as np
 from pyproj import CRS, Transformer
+import math
 
 
 
@@ -33,16 +34,19 @@ def get_path_parts(path, drogi):
 
         if key1 in drogi:
             path_part = drogi[key1]
-            for j in range(len(path_part)):
-                path_part[j] = translate_path_part_to_leaflet(path_part[j], transformer)
-            path_parts.append(path_part)
+            translated_path_part = [
+                translate_path_part_to_leaflet(part, transformer) for part in path_part
+            ]
+            path_parts.append(translated_path_part)
         elif key2 in drogi:
             path_part = drogi[key2]
-            for j in range(len(path_part)):
-                path_part[j] = translate_path_part_to_leaflet(path_part[j], transformer)
-            path_parts.append(path_part)
+            translated_path_part = [
+                translate_path_part_to_leaflet(part, transformer) for part in path_part
+            ]
+            path_parts.append(translated_path_part)
         else:
             raise KeyError(f"Neither {key1} nor {key2} found in drogi")
+
     return path_parts
 
 
@@ -94,4 +98,59 @@ def find_closest_node(x_start, y_start, graph):
                 return [x, y]
         i+=1
         
+def create_closest_point(y_start, x_start, first_node, second_node, roads):
+    
+    def calculate_slope(x1, y1, x2, y2):
+        return (y2 - y1) / (x2 - x1)
+    
+    def distance_from_point_to_line(x0, y0, point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        m = calculate_slope(x1, y1, x2, y2)
+        b = y1 - m * x1
+        return abs(m * x0 - y0 + b) / math.sqrt(m ** 2 + 1)
+    
+    def closest_point_on_line(x0, y0, x1, y1, m):
+        x_proj = x0 - ((x0 - x1) * m + (y0 - y1)) / (1 + m ** 2)
+        y_proj = y0 + m * (x0 - x_proj)
+        return x_proj, y_proj
+
+    key_road1 = f"{first_node}_{second_node}"
+    key_road2 = f"{second_node}_{first_node}"
+    if key_road1 in roads:
+        whole_edge = roads[key_road1]
+        print(whole_edge, "->1")
+    elif key_road2 in roads:
+        whole_edge = roads[key_road2]
+        print(whole_edge, "->2")
+    else:
+        print("nie znalazlo")
+    
+    min_distance = 9999999999999
+    #x1, y1, x2, y2 = None
+    for i in range(len(whole_edge) - 1):
+        point1 = whole_edge[i]
+        point2 = whole_edge[i + 1]
+        dist = distance_from_point_to_line(x_start, y_start, point1, point2)
+        print(dist)
+        if dist<min_distance:
+            min_distance = dist
+            x1, y1 = point1
+            x2, y2 = point2
+            print("znalezione: ", min_distance)
+    m = calculate_slope(x1, y1, x2, y2)
+    point0 = closest_point_on_line(x_start, y_start, x1, y1, m)
+
+    cs2180 = CRS.from_proj4(
+        "+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +units=m +no_defs +type=crs")
+    cs4326 = CRS.from_epsg(4326)
+    transformer = Transformer.from_crs(cs2180, cs4326)
+    point0 = translate_path_part_to_leaflet(point0, transformer)
+    print(point0)
+    return point0
+
+    
+
+        
+
 
